898e6fff36cde263750b74cc8b331bb52df3a379
Bejamin Schachter <benschac@gmail.com>
2016-12-12 12:17:29 -0500

    adds css-concat and fixes variable issues in build

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 const sass = require('gulp-sass');
-
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
+const concat = require('gulp-concat-css');
 
 
 
@@ -10,19 +12,34 @@ gulp.task('default', ['serve'], () => {
   console.log('testing');
 });
 
-gulp.task('serve', ['sass'], () => {
+gulp.task('serve', ['sass:watch', 'html:watch'], () => {
   browserSync.init({
     server: '.'
   })
+});
 
-    gulp.watch("src/styles/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+gulp.task('clean', function() {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  return del(['build']);
 });
 
 
-gulp.task('sass', () => {
+gulp.task('sass', ['clean'], () => {
   return gulp.src('src/styles/*.scss')
-             .pipe(sass())
-             .pipe(gulp.dest('dist/styles/styles'))
+             .pipe(sass().on('error', sass.logError))
+             .pipe(autoprefixer({
+               browsers: ['last 2 versions'],
+               cascade: false
+             }))
+             .pipe(concat("./style.css"))
+             .pipe(gulp.dest('./dist/styles/'))
              .pipe(browserSync.stream());
+})
+
+gulp.task('sass:watch',() => {
+  gulp.watch("src/styles/*.scss", ['sass']);
+});
+
+gulp.task('html:watch', () => {
+  gulp.watch("*.html").on('change', browserSync.reload);
 })
