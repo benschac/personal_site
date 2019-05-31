e2e6a82cff9b383e86201dab674d3d56daab8189
Benjamin Schachter <b.schachter@prolificinteractive.com>
2019-05-31 10:11:44 -0400

    add: react + react dom

---
+++ src/derp.js
@@ -0,0 +1,3 @@
+export default function derp() {
+  console.log('hiiiiii');
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import {render} from 'react-dom';
+import derp from './derp';
+import './styles/index.global.css';
+
 class Benjamin {
   hello() {
-    console.log('hello');
+    console.log(derp());
   }
 }
 
-export default Benjamin;
+const ben = new Benjamin();
+
+ben.hello();
+
+render(<div>Hello World again and again</div>, document.getElementById('app'));
