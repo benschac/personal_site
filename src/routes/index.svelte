03b54ff72be46cd51da77db95eb2baf9ac865a56
Bejamin Schachter <benschac@gmail.com>
2020-05-14 08:11:46 -0400

    add svg and stub

---
+++ src/routes/index.svelte
@@ -0,0 +1,42 @@
+
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Benjamin Schachter</title>
+</svelte:head>
+
+<h2>Under Construction</h2>
+<h3>Currently looking for my next role</h3>
+<h3>Reach out over <a href="http://twitter.com/benschac">social</a></h3>
