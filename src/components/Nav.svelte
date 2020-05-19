4df8bf758832eb52f4960a2f90de7efd16e4bc4e
Bejamin Schachter <benschac@gmail.com>
2020-05-19 15:30:53 -0400

    cleanup: unused code

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -70,10 +70,5 @@
 <nav>
 	<ul>
 		<li class="icon"><a aria-current='{segment === undefined ? "page" : undefined}' href='.'><img src="ben-filled.svg" alt=""></a></li>
-		<!-- <li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li> -->
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<!-- <li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li> -->
 	</ul>
 </nav>
