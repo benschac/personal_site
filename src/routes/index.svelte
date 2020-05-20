b47590168dd73a8932250b6e32f8a4690c0da5f8
Bejamin Schachter <benschac@gmail.com>
2020-05-19 22:40:26 -0400

    add: og tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,10 @@
 <svelte:head>
-	<title>Benjamin Schachter</title>
+  <title>Benjamin Schachter</title>
 </svelte:head>
 
 <h2>Under Construction</h2>
 <h3>Currently looking for my next role</h3>
-<h3>Reach out over <a href="http://twitter.com/benschac">social</a></h3>
+<h3>
+  Reach out over
+  <a href="http://twitter.com/benschac">social</a>
+</h3>
