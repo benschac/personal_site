1337695a16df8377c604a34a9dd58851ccbaf9de
Benjamin Schachter <benschac@gmail.com>
2020-06-25 09:20:22 -0400

    update:work

--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -6,6 +6,7 @@
 .nav {
   margin: var(--s2) 0;
   display: flex;
+  margin: var(--s2);
 }
 
 
@@ -16,24 +17,26 @@
 
 .work {
   min-height: 100vh;
-  margin: 0 var(--s2);
+  margin: 0 auto;
+  max-width: 80rem;
 }
 
 .grid {
   display: grid;
-  /* minmax dis */
   grid-auto-rows: auto;
   grid-template-columns: repeat(2, minmax( var(--s4), 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(30ch, 1fr));
   gap: var(--s-3);
-  place-content: center;
+  margin: 0 var(--s2);
 }
 
 .grid__item {
   border: var(--s-3) solid var(--text-primary);
   display: flex;
-  padding: var(--s0);
   justify-content: center;
   align-items: center;
+  text-align: center;
+  padding: var(--s-1) 0;
 }
 
 
